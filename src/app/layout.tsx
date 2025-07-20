aeb7e511e8d9f291ef2608a1fb2cc01c681d4fdd
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-20 14:03:27 +0530

    adding the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Talynq",
   description: "Talynq is a modern job creation and talent discovery platform that connects skilled individuals with the right opportunities — faster, smarter, and easier.",
+  icons: {
+    icon: "/talynq/talynq-icon.png",
+    shortcut: "/talynq/talynq-icon.png",
+    apple: "/talynq/talynq-icon.png",
+  }
 };
 
 export default function RootLayout({
