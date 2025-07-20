756f2f105942fd86f3a1446f30e6777e2608adf4
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-20 17:23:32 +0530

    adding the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className='p-4 flex justify-between items-center'>
+      <Link href="/"><Image src="/talynq/talynq-text.png" alt="Logo" width={100} height={100} /></Link>
+
+      <Button variant={"outline"} className='cursor-pointer'>Login</Button>
+    </nav>
   )
 }
 
