46640a4e6f37912246849a6d927402d5df6a8538
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-21 09:34:37 +0530

    changing the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
       <section>
-        <h1>Find your dream job </h1>
+        <h1>Find your dream job <span className='flex'>and get <Image src={"/talynq/placed.png"} className='h-10 w-auto' height={100} width={100} alt='Placed logo'/></span></h1>
       </section>
     </div>
   )
