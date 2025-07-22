2cd51131fc4711cb8d0162cb96f39f1f8d3b288f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-22 09:27:52 +0530

    add the api for fetching the jobs

---
+++ src/app/api/jobs/route.ts
@@ -0,0 +1,93 @@
+import { db } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const user = await currentUser();
+        
+        if (!user) {
+            return NextResponse.json(
+                { error: "User not authenticated." },
+                { status: 401 }
+            );
+        }
+        
+        const {location, company_id, searchQuery} = await request.json();
+        const filters: object[] = [];
+
+        if (location) {
+        filters.push({
+            location: {
+            contains: location,
+            mode: "insensitive"
+            }
+        });
+        }
+
+        if (company_id) {
+        filters.push({
+            company_id: company_id
+        });
+        }
+
+        if (searchQuery) {
+        filters.push({
+            OR: [
+            {
+                title: {
+                contains: searchQuery,
+                mode: "insensitive"
+                }
+            },
+            {
+                description: {
+                contains: searchQuery,
+                mode: "insensitive"
+                }
+            },
+            {
+                requirements: {
+                contains: searchQuery,
+                mode: "insensitive"
+                }
+            }
+            ]
+        });
+        }
+
+        const whereClause = filters.length > 0 ? { OR: filters } : undefined;
+
+        const jobs = await db.jobs.findMany({
+        where: whereClause,
+        include: {
+            recruiter: true,
+            company: true
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+        });
+
+        if (jobs) {
+            return NextResponse.json(jobs, {
+                status: 200
+            });
+        }
+
+        return NextResponse.json(
+            { error: "No jobs found." },
+            { status: 404 }
+        );
+
+    } catch (error: unknown) {
+        return NextResponse.json(
+            {
+                error: (error as Error).message || "Unknown error occurred while fetching jobs."
+            },
+            {
+                status: 500
+            }
+        );
+    }
+}
