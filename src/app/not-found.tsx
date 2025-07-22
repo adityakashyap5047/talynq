65e07be2395b27baf12466c9723cbcefe89648e1
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-22 19:33:47 +0530

    adding the not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center bg-gradient-to-br text-white px-6 py-12 text-center">
+
+      <h1 className="text-8xl font-extrabold tracking-tight gradient-title my-12">
+        404
+      </h1>
+
+      <h2 className="text-2xl sm:text-4xl font-semibold mb-2">
+        Page Not Found
+      </h2>
+
+      <p className="text-gray-300 max-w-md mb-6">
+        The page you{"'"}re looking for doesn{"'"}t exist or has been moved. You might have followed a broken link or mistyped the URL.
+      </p>
+
+      <Link href="/">
+        <Button variant="blue" className="flex items-center gap-2">
+          <ArrowLeft size={18} />
+          Back to Home
+        </Button>
+      </Link>
+    </div>
+  );
+}
