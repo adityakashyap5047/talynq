7e41e05812cabacbb6ca43492fe4cdd9e30fb0f4
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-23 08:08:14 +0530

    created the api for fetching the job

---
+++ src/app/api/jobs/[id]/route.ts
@@ -0,0 +1,35 @@
+import { db } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
+    
+    try {
+
+        const id = await params.id;
+        const user = await currentUser();
+
+        if (!user) {
+            return new Response("Unauthorized", { status: 401 });
+        }
+
+        const job = await db.jobs.findUnique({
+            where: {
+                id: id
+            },
+            include: {
+                company: true,
+                applications: true
+            }
+        })
+
+        if (job) {
+            return NextResponse.json(job, { status: 200 });
+        }
+
+        return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    } catch (error) {
+        return NextResponse.json({ error: (error as Error).message || "Internal Server Error" }, { status: 500 });
+    }
+    
+}
